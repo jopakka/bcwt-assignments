b735b8c0d93a8f5ea9901f245cd534f4c8de3cf1
jopakka <jopakkamies@gmail.com>
2020-10-23 16:40:21 +0300

    get json

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -2,25 +2,8 @@
 // catRoute
 const express = require('express');
 const router = express.Router();
+const catController = require('../controllers/catController');
 
-router.get('/', (req, res) => {
-  res.send('From this endpoint you can get cats.');
-});
-
-router.get('/:id', (req, res) => {
-  res.send(`You requested a cat whose id is ${req.params.id}`);
-});
-
-router.post('/', (req, res) => {
-  res.send('With this endpoint you can add cats.');
-});
-
-router.put('/', (req, res) => {
-  res.send('With this endpoint you can edit cats.');
-});
-
-router.delete('/', (req, res) => {
-  res.send('With this endpoint you can delete cats.');
-});
+router.get('/', catController.cat_list_get);
 
 module.exports = router;
