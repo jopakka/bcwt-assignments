88d97f084be39c308c3e22b9a22c7833d5403aab
jopakka <jopakkamies@gmail.com>
2020-10-23 16:55:04 +0300

    get cat by id

--- controllers/catController.js
+++ controllers/catController.js
@@ -8,6 +8,12 @@ const cat_list_get = (req, res) => {
   res.json(cats);
 };
 
+const cat_get = (req, res) => {
+  const oneCat = cats.filter(cat => cat.id === req.params.id).reduce(cat => cat);
+  res.json(oneCat);
+};
+
 module.exports = {
   cat_list_get,
+  cat_get
 };
