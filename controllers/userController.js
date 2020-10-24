07e380e23c4ca717c5f07f2798d80467b63d875a
jopakka <jopakkamies@gmail.com>
2020-10-24 09:44:22 +0300

    delete password property

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,14 +5,23 @@ const userModel = require('../models/userModel');
 const users = userModel.users;
 
 const user_list_get = (req, res) => {
+  for (const i in users) {
+    deleteKey(users[i], 'password');
+  }
   res.json(users);
 };
 
 const user_get = (req, res) => {
   const oneUser = users.filter(user => user.id === req.params.id).
       reduce(user => user);
+  deleteKey(oneUser, 'password');
   res.json(oneUser);
 };
+
+function deleteKey(user, key) {
+  delete user[key];
+  return user;
+}
 
 module.exports = {
   user_list_get,
