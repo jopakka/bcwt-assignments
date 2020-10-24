63b8b07fcf66011b2ac110cb0a45fdb0a9f5a8cb
jopakka <jopakkamies@gmail.com>
2020-10-24 10:33:48 +0300

    file upload work
    Routing and Middleware complete

--- controllers/catController.js
+++ controllers/catController.js
@@ -9,11 +9,18 @@ const cat_list_get = (req, res) => {
 };
 
 const cat_get = (req, res) => {
-  const oneCat = cats.filter(cat => cat.id === req.params.id).reduce(cat => cat);
+  const oneCat = cats.filter(cat => cat.id === req.params.id).
+      reduce(cat => cat);
   res.json(oneCat);
+};
+
+const cat_post = (req, res) => {
+  console.log(req.file);
+  res.json(req.file);
 };
 
 module.exports = {
   cat_list_get,
-  cat_get
+  cat_get,
+  cat_post,
 };
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -3,8 +3,12 @@
 const express = require('express');
 const router = express.Router();
 const catController = require('../controllers/catController');
+const multer = require('multer');
+const upload = multer({dest: './uploads/'});
 
 router.get('/', catController.cat_list_get);
 router.get('/:id', catController.cat_get);
+
+router.post('/', upload.single('uploaded_file'), catController.cat_post);
 
 module.exports = router;
