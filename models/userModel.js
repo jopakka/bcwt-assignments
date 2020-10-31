7ce2f55aa2859f0970922d4d5a00d5df29f3b0c6
jopakka <jopakkamies@gmail.com>
2020-10-31 17:29:12 +0200

    test

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 'use strict';
 const pool = require('../database/db');
 const promisePool = pool.promise();
-const { validationResult } = require('express-validator')
+const {validationResult} = require('express-validator');
 
 const getAllUsers = async () => {
   try {
@@ -9,7 +9,7 @@ const getAllUsers = async () => {
         'SELECT user_id, name, email FROM wop_user');
     return rows;
   } catch (e) {
-    return { error: e.message };
+    return {error: e.message};
   }
 };
 
@@ -17,25 +17,25 @@ const getUser = async (id) => {
   try {
     const [oneUser] = await promisePool.execute(
         'SELECT user_id, name, email FROM wop_user WHERE user_id = ?', [id]);
-    return oneUser.reduce(user => user, {xD: 'ggez'});
+    return oneUser[0] //|| {error: `No users with id: ${id}`};
   } catch (e) {
-    return { error: e.message };
+    return {error: e.message};
   }
 };
 
 const addUser = async (req) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty())
-    return { error: errors.array() };
+  if (!errors.isEmpty())
+    return {error: errors.array()};
 
   try {
     const [status] = await promisePool.execute(
         'INSERT INTO wop_user(name, email, password) VALUES(?, ?, ?)',
         [req.body.name, req.body.email, req.body.passwd]);
-    const user = await getUser(status['insertId'])
-    return user
+    const user = await getUser(status['insertId']);
+    return user;
   } catch (e) {
-    return { error: e.message };
+    return {error: e.message};
   }
 };
 
