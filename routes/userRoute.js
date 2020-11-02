c998250bd3e6aaa46b16c2326a815ee81fd6325a
jopakka <jopakkamies@gmail.com>
2020-11-02 16:47:41 +0200

    before cookies

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -12,7 +12,8 @@ router.post('/', [
   body('name').trim().escape().isLength({min: 3}),
   body('email').isEmail().normalizeEmail(),
   body('passwd').
-      isLength({min: 8}).withMessage('Password must contain at least 8 characters').
+      isLength({min: 8}).
+      withMessage('Password must contain at least 8 characters').
       custom(v => v.toLowerCase() !== v).
       withMessage('Password must have at least one capital letter'),
 ], userController.user_create_post);
