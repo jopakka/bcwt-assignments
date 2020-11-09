8e09788a0bb9900766c5dc8b2ed7a306cb70eb12
jopakka <jopakkamies@gmail.com>
2020-11-09 10:02:53 +0200

    class stuff

--- utils/resize.js
+++ utils/resize.js
@@ -5,8 +5,11 @@ const makeThumbnail = async (file, thumbname) => {
   // file = full path to image (req.file.path), thumbname = filename (req.file.filename)
   // TODO: use sharp to create a png thumbnail of 160x160px, use async await
   try {
-    const image = await sharp(file).resize(160, 160).toBuffer();
-    console.log('resize', image)
+    const image = await sharp(file).resize(160, 160).toFile(thumbname, (err, info) => {
+      console.log('resize info', info)
+      console.error('resize err', err)
+    });
+    console.log('resize image', image)
   } catch (e) {
     console.error('resize', e.message);
   }
