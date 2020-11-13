90a24119a041946f21ed422c1c3b497e37f11f04
jopakka <jopakkamies@gmail.com>
2020-11-13 10:47:16 +0200

    https

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const express = require('express');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 const authRoute = require('./routes/authRoute');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const passport = require('./utils/pass');
+
+const sslkey = fs.readFileSync('ssl-key.pem');
+const sslcert = fs.readFileSync('ssl-cert.pem')
+
+const options = {
+  key: sslkey,
+  cert: sslcert
+};
 const app = express();
 const port = 3000;
+const httpsPort = 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,4 +32,8 @@ app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 app.use('/thumbnails', express.static('thumbnails'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+https.createServer(options, app).listen(httpsPort, () => console.log(`App is listening https port ${httpsPort}`));
+http.createServer((req, res) => {
+  res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
+  res.end();
+}).listen(port, () => console.log(`App is listening port ${port} and redirect it to port ${httpsPort}`));
