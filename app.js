efa7cc0052557bff4293e017582eb8f8d12d41c2
jopakka <jopakkamies@gmail.com>
2020-11-13 11:20:07 +0200

    https for localhost

--- app.js
+++ app.js
@@ -3,37 +3,34 @@ const express = require('express');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 const authRoute = require('./routes/authRoute');
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const passport = require('./utils/pass');
 
-const sslkey = fs.readFileSync('ssl-key.pem');
-const sslcert = fs.readFileSync('ssl-cert.pem')
-
-const options = {
-  key: sslkey,
-  cert: sslcert
-};
 const app = express();
 const port = 3000;
 const httpsPort = 8000;
+
+app.enable('trust proxy');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+}
+app.get('/', (req, res) => {
+  res.send('Hello Secure World!');
+});
+
+// Routes
 app.use(express.static('public_html'));
 app.use(express.static('uploads'));
 app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 app.use('/thumbnails', express.static('thumbnails'));
-
-https.createServer(options, app).listen(httpsPort, () => console.log(`App is listening https port ${httpsPort}`));
-http.createServer((req, res) => {
-  res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
-  res.end();
-}).listen(port, () => console.log(`App is listening port ${port} and redirect it to port ${httpsPort}`));
